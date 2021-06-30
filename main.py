ed418d89380f85973d9a529b258c24595b742b57
Israel Sagesse <israelsagesse@gmail.com>
2021-06-30 11:22:10 -0400

    using the tkinter grid system

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import tkinter
 
-window = tkinter.Tk()
-window.title("My first GUI Program")
-window.minsize(width=500, height=300)
-
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24))
-my_label.pack()
-
-#button
 def button_clicked():
     my_label.config(text=input.get())
 
+window = tkinter.Tk()
+window.title("My first GUI Program")
+window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
+
+#label
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24))
+my_label.grid(column=0, row=0)
+
+#button
 button = tkinter.Button(text="Click ME", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
+
+bt = tkinter.Button(text="New Button")
+bt.grid(column=2, row=0)
 
 # Entry
 input = tkinter.Entry(width=10)
-input.pack()
+input.grid(column=3, row=2)
 
 
 window.mainloop()
